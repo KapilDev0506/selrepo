3c07e98a23fbda9a15e7577869ece87b296aa930
shiva-kant <shivakant.choudhary@gmail.com>
2022-09-19 14:48:26 +0530

    first commit

---
+++ SelDemo/src/main/java/MyMaths.java
@@ -0,0 +1,7 @@
+
+public class MyMaths {
+
+	public int add(int x, int y) {
+		return x+y;
+	}
+}
---
+++ SelDemo/src/test/java/NewTest.java
@@ -0,0 +1,42 @@
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import static org.testng.Assert.assertEquals;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	
+	WebDriver webDriver;
+	WebElement webElement;
+  @Test
+  public void chromeTest()
+  {
+	  String path="//*[@id=\"p\"]";
+	webElement=  webDriver.findElement(By.xpath(path));
+	String response=webElement.getText();
+	assertEquals("Hello world", response);
+  }
+  
+  
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\shivchou\\Desktop\\seldemo\\chromedriver.exe");
+	  webDriver =new ChromeDriver();
+	  webDriver.get("C:\\Users\\shivchou\\Desktop\\seldemo\\hello.html");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	 webDriver.close();
+  }
+
+}
