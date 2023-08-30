0ee0c0353ed9ec6729f04b16a36febef1f9e3a28
KapilDev0506 <143598962+KapilDev0506@users.noreply.github.com>
2023-08-30 17:44:24 +0530

    Update MyMaths.java

--- SelDemo/src/main/java/MyMaths.java
+++ SelDemo/src/main/java/MyMaths.java
@@ -1,7 +1,19 @@
+package com.mycompany.app;
 
-public class MyMaths {
+/**
+ * Hello world!
+ */
+public class App {
 
-	public int add(int x, int y) {
-		return x+y;
-	}
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
 }
