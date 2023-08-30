b0cc9938103f0ea99aac937052a05a7d3b64f837
KapilDev0506 <143598962+KapilDev0506@users.noreply.github.com>
2023-08-30 17:53:05 +0530

    Update NewTest.java

--- SelDemo/src/test/java/NewTest.java
+++ SelDemo/src/test/java/NewTest.java
@@ -5,19 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Unit test for simple App.
  */
-public class NewTest.java
+public class NewTest
 {
     @Test
     public void testAppConstructor() {
-        App app1 = new App();
-        App app2 = new App();
+    	MyMaths app1 = new MyMaths();
+    	MyMaths app2 = new MyMaths();
         assertEquals(app1.getMessage(), app2.getMessage());
     }
 
     @Test
     public void testAppMessage()
     {
-        App app = new App();
+    	MyMaths app = new MyMaths();
         assertEquals("Hello World!", app.getMessage());
     }
 }
